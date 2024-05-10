133c840158fa4b36aa98c2d46ebc0e053ba2d13f
Younggue Bae <ybae2@lululemon.com>
2024-05-09 19:13:39 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""Builds the package.
+"""
+import os
+import sys
+import pkg_resources
+from setuptools import setup, find_packages
+
+
+VERSION = "0.0.1"
+DESCRIPTION = "temporal-fusion-transformer"
+
+setup(
+    name="nextmining-forecasting",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="unknown",
+    # author_email="<louiezzang@gmail.com>",
+    package_dir={"": "src"},
+    # url="https://github.com/louiezzang/nextmining-forecasting",
+    keywors=[
+        "Temporal Fusion Transformer",
+        "Forecast",
+    ],
+    packages=find_packages(where="src"),
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+)
