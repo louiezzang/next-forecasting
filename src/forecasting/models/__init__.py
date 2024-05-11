73012265caae29446b314196b75a563cbc92a572
Younggue Bae <louiezzang@gmail.com>
2024-05-11 10:48:19 -0700

    Added DeepAR model

--- src/forecasting/models/__init__.py
+++ src/forecasting/models/__init__.py
@@ -1,2 +1,4 @@
-from .temporal_fusion_transformer import TemporalFusionTransformer
+from .temporal_fusion_transformer.net import TemporalFusionTransformer
 from .temporal_fusion_transformer.modules import QuantileLoss
+from .deepar.net import DeepAR
+from .deepar.LSTM import LSTM
